78059663cac371c1a3b35979eea5c68ffdd4fc02
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-07-02 02:14:01 -0300

    initial commit

---
+++ SwiftCustomView/CustomView.swift
@@ -0,0 +1,31 @@
+//
+//  CustomView.swift
+//  SwiftCustomView
+//
+//  Created by Marcelo on 7/2/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    // MARK: - Outlets
+    
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        _ = Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)?[0] as! UIView
+        
+        
+        
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    
+}
